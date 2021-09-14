5be660cf59c771bedb339a3453bb98aac409fc63
oscar0812 <oscar0812torres@gmail.com>
2021-09-14 11:08:01 -0500

    added wysiswyg to short_description

--- app/Http/Controllers/Admin/CvEntryCrudController.php
+++ app/Http/Controllers/Admin/CvEntryCrudController.php
@@ -21,7 +21,7 @@ class CvEntryCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,7 +33,7 @@ class CvEntryCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
@@ -50,13 +50,13 @@ class CvEntryCrudController extends CrudController
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -68,20 +68,28 @@ class CvEntryCrudController extends CrudController
         CRUD::field('cv_section_id');
         CRUD::field('title');
         CRUD::field('place_name');
-        CRUD::field('short_description');
+        CRUD::addField(['name'=>'short_description',
+            'type'  => 'tinymce',
+            // optional overwrite of the configuration array
+            'options' => [
+                'plugins' => 'lists,image,link,media,anchor,autolink,imagetools,autoresize,table,codesample,code',
+                'toolbar' => 'undo redo formatselect fontsizeselect bullist numlist indent link image bold italic underline forecolor backcolor table anchor codesample code',
+                'imagetools_cors_hosts'=> [ 'homestead.com', 'oscarrtorres.com' ],
+            ],
+        ]);
         CRUD::field('start_date');
         CRUD::field('end_date');
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
